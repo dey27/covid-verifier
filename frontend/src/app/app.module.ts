43b88eae0a58c9253aa44bd5157ddab35710f65f
Prithviraj Dey <prithviraj.dey95@gmail.com>
2021-04-24 21:09:17 +0530

    Updating Angular Versions & Related Packages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
-import { MatSidenavModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HomeComponent } from './components/home/home.component';
 
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    
+    BrowserModule,    
     AppRoutingModule
   ],
   providers: [],
