cdaa665edc9f6c4ce6ea3bbae5eb3d1cb3a0c7e5
Prithviraj Dey <prithviraj.dey95@gmail.com>
2021-04-25 19:04:43 +0530

    Added mockdata and basic functions

--- frontend/src/app/covid-verifier-dataportal/services/mock-test-data.service.ts
+++ frontend/src/app/covid-verifier-dataportal/services/mock-test-data.service.ts
@@ -16,16 +16,17 @@ export class MockTestDataService {
       {
         "post_id": 1,
         "labels": "beds, oxygen",
-        "post_name": "test1",
+        "post_name": "Oxygen Delivery",
         "phone_numbers": [
+          "9425331581",
           "9425331581"
         ],
-        "description": "abc",
+        "description": "Oxygen tank delivery",
         "location_city": "delhi",
-        "location_area": null,
+        "location_area": "paharganj",
         "supporting_url": null,
-        "up_votes_total": 0,
-        "down_votes_total": 0,
+        "up_votes_total": 10,
+        "down_votes_total": 5,
         "date_created": "2021-04-25T12:27:35.193850+05:30"
       }
     ];
